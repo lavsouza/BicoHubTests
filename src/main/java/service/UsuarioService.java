d1c4609c189d42f272b9c734cc059ffb1d983cb0
lavsouza <lucascoder14@gmail.com>
2026-01-20 20:56:03 -0300

    Negocio

--- src/main/java/service/UsuarioService.java
+++ src/main/java/service/UsuarioService.java
@@ -1,11 +1,30 @@
 package main.java.service;
 
+import main.java.model.Usuario;
 import main.java.respositorie.UsuarioRepository;
 
 public class UsuarioService {
 
     protected UsuarioRepository usuarioRepository;
+
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
+
+    public UsuarioService() {
+        Usuario usuarioPadrao = new Usuario(
+                "Usuario Padrao",
+                "user@sistema.com",
+                "12345678",
+                "00000000000",
+                "Endereço padrão"
+        );
+
+        this.salvar(usuarioPadrao);
+    }
+
+    public void salvar(Usuario usuario) {
+        this.usuarioRepository.salvar(usuario);
+    }
+
 }
--- teste/main/test/UsuarioServiceTeste.java
+++ teste/main/test/UsuarioServiceTeste.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.java.Usuario;
-import main.java.UsuarioNegocio;
 import main.java.UsuarioRepositorio;
 
 public class UsuarioTeste {
