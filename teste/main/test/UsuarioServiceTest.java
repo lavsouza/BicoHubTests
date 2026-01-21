deecdaa62794d1b1a72a26076ac63b54713961d1
lavsouza <lucascoder14@gmail.com>
2026-01-20 21:54:33 -0300

    Teste Exclusão e Criação de Usuario

--- teste/main/test/UsuarioServiceTest.java
+++ teste/main/test/UsuarioServiceTest.java
@@ -13,22 +13,16 @@ import main.java.model.Usuario;
 import main.java.respositorie.UsuarioRepository;
 
 @ExtendWith(MockitoExtension.class)
-public class UsuarioServiceTeste {
+public class UsuarioServiceTest {
 
     @Mock
     private UsuarioRepository usuarioRepository;
 
     @InjectMocks
     private UsuarioService usuarioService;
-
-    @BeforeEach
-	public void initTestes() {
-		this.rep = mock(UsuarioRepository.class);
-	}
 	
 	@Test
-	public void cadastrarUsuario() {
-
+	public void testeCadastrarUsuarioDadosValidos() {
         Usuario u = new Usuario("Vilmar", "teste@gmail.com", "12345678", "12345678910", "EnderecoTeste");
         UsuarioService usuarioService = new UsuarioService(new UsuarioRepository());
 
@@ -36,4 +30,19 @@ public class UsuarioServiceTeste {
 
         Assertions.assertNotNull(uCad);
 	}
+
+    @Test
+    void deveExcluirUsuarioQuandoCpfExiste() {
+
+        String cpf = "12345678901";
+
+        when(usuarioRepository.existePorCpf(cpf)).thenReturn(true);
+
+        Assertions.assertDoesNotThrow(() ->
+                usuarioService.deletarPorCpf(cpf)
+        );
+
+        verify(usuarioRepository, times(1)).deletarPorCpf(cpf);
+    }
+
 }
