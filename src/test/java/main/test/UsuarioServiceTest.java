b9415997b80899e77b8efcace45a7b308907d50b
tuliobernardino <bernardinotulio@gmail.com>
2026-01-20 23:56:54 -0300

    adicionados casos faltantes

--- src/test/java/main/test/UsuarioServiceTest.java
+++ src/test/java/main/test/UsuarioServiceTest.java
@@ -47,4 +47,63 @@ public class UsuarioServiceTest {
         verify(usuarioRepository, times(1)).deletarPorCpf(cpf);
     }
 
+    @Test
+    void deveDetectarUsuarioComNomeNulo() {
+        Usuario u = new Usuario(null, "abc12345@gmail.com", "12345678", "12345678901", "enderecoTeste");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> usuarioService.salvar(u));
+
+        verify(usuarioRepository, never()).salvar(any(Usuario.class));
+    }
+
+    @Test
+    void deveImpedirCadastroDeUsuarioComEmailExistente() {
+        Usuario a = new Usuario("Joao", "teste@gmail.com", "12345678", "12345678901", "EnderecoTeste");
+        usuarioService.salvar(a);
+        Usuario b = new Usuario("Maria", "teste@gmail.com","87654321","10987654321","OutroEndereco");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> usuarioService.salvar(b));
+
+        verify(usuarioRepository, never()).salvar(any(Usuario.class));
+    }
+
+    @Test
+    void deveImpedirCadastroDeUsuarioComCpfExistente() {
+        Usuario a = new Usuario("Joao", "teste@gmail.com", "12345678", "12345678901", "EnderecoTeste");
+        usuarioService.salvar(a);
+        Usuario b = new Usuario("Maria", "testado@gmail.com","87654321","12345678901","OutroEndereco");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> usuarioService.salvar(b));
+
+        verify(usuarioRepository, never()).salvar(any(Usuario.class));
+    }
+
+    @Test
+    void deveImpedirCadastroDeUsuarioComCpfInvalido() {
+        Usuario a = new Usuario("Joao", "teste@gmail.com", "12345678", "12", "EnderecoTeste");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> usuarioService.salvar(a));
+
+        verify(usuarioRepository, never()).salvar(any(Usuario.class));
+    }
+
+    @Test
+    void deveImpedirCadastroComSenhaFraca() {
+        Usuario a = new Usuario("Joao", "teste@gmail.com", "123", "12345678901", "EnderecoTeste");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> usuarioService.salvar(a));
+
+        verify(usuarioRepository, never()).salvar(any(Usuario.class));
+    }
+
+    @Test
+    void deveImpedirCadastroComEnderecoVazio() {
+        Usuario a = new Usuario("Joao", "teste@gmail.com", "12345678", "12345678901", null);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> usuarioService.salvar(a));
+
+        verify(usuarioRepository, never()).salvar(any(Usuario.class));
+    }
+
+
 }
